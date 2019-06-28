f51a80539b9719e6371ae2770fb9603524e597e8
Francisco Costa <francisco@fcosta.pt>
2019-06-28 09:55:47 +0100

    Created pages folder and first etst pages. Routes are working.

---
+++ .next/static/development/pages/show.js
@@ -0,0 +1,86 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/show.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fshow&absolutePagePath=%2FUsers%2FFrancisco%2FDocuments%2FFlatiron%2FProject%2Fethereum-fundraiser%2Fpages%2Fshow.js!./":
+/*!**************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fshow&absolutePagePath=%2FUsers%2FFrancisco%2FDocuments%2FFlatiron%2FProject%2Fethereum-fundraiser%2Fpages%2Fshow.js ***!
+  \**************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/show", function() {
+      var page = __webpack_require__(/*! ./pages/show.js */ "./pages/show.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/show.js */ "./pages/show.js", function() {
+          if(!next.router.components["/show"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/show.js */ "./pages/show.js")
+          next.router.update("/show", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_1aef2d0bbc0d334d831c ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_1aef2d0bbc0d334d831c */ "dll-reference dll_1aef2d0bbc0d334d831c"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/show.js":
+/*!***********************!*\
+  !*** ./pages/show.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/Francisco/Documents/Flatiron/Project/ethereum-fundraiser/pages/show.js";
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "You can check all projects here.");
+});
+
+/***/ }),
+
+/***/ 1:
+/*!******************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fshow&absolutePagePath=%2FUsers%2FFrancisco%2FDocuments%2FFlatiron%2FProject%2Fethereum-fundraiser%2Fpages%2Fshow.js ***!
+  \******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fshow&absolutePagePath=%2FUsers%2FFrancisco%2FDocuments%2FFlatiron%2FProject%2Fethereum-fundraiser%2Fpages%2Fshow.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fshow&absolutePagePath=%2FUsers%2FFrancisco%2FDocuments%2FFlatiron%2FProject%2Fethereum-fundraiser%2Fpages%2Fshow.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_1aef2d0bbc0d334d831c":
+/*!*******************************************!*\
+  !*** external "dll_1aef2d0bbc0d334d831c" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_1aef2d0bbc0d334d831c;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=show.js.map
