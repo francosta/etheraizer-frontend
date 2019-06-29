57e8f6ed2dbd9644bc55d62de5b95353fb3a7707
Francisco Costa <francisco@fcosta.pt>
2019-06-29 09:17:45 +0100

    Added styling to footer.

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "df65834eaa8a63fbd949";
+/******/ 	var hotCurrentHash = "0850df2e62cf2e01a987";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
