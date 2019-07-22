e93a56a4b3bb8a39e531ca158dfdcbfcb722c96e
francosta <francisco@fcosta.pt>
2019-07-22 11:58:02 +0100

    .gitignore fix

--- ethereum/projectContract.js
+++
@@ -1,7 +0,0 @@
-import web3 from "./web3";
-import Fundraise from "./build/Fundraise.json";
-
-// This is a function that will create a 'Fundraise' contract for each address that it's passed onto it
-export default address => {
-  return new web3.eth.Contract(JSON.parse(Fundraise.interface), address);
-};
--- pages/show.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default () => {
-  return <h1>You can check all projects here.</h1>;
-};
